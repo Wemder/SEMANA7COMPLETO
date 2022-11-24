220e069708eb7877f3bd3048cb71a62ccb8ba0d1
Bryan_Chicaiza <bchicaiza@ec.krugercorp.com>
2022-11-23 21:35:13 -0500

    Nuevas tablas

---
+++ SEMANA7COMPLETO/modelos/Especialidad.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEMANA7COMPLETO.modelos
+{
+    public class Especialidad
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idEspecialidad { get; set; }
+        
+        [MaxLength(50)]
+        public string especialidad { get; set; }
+        
+        [MaxLength(50)]
+        public string descripcion { get; set; }
+    }
+}
---
+++ SEMANA7COMPLETO/modelos/Paciente.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEMANA7COMPLETO.modelos
+{
+    public class Paciente
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idPaciente { get; set; }
+
+        [MaxLength(50)]
+        public string primerNombre { get; set; }
+
+        [MaxLength(50)]
+        public string segundoNombre { get; set; }
+
+        [MaxLength(50)]
+        public string primerApellido { get; set; }
+
+        [MaxLength(50)]
+        public string segundoApellido { get; set; }
+
+        [MaxLength(50)]
+        public string email { get; set; }
+
+        [MaxLength(50)]
+        public string numeroContacto { get; set; }
+
+        [MaxLength(50)]
+        public string clave { get; set; }
+
+        [MaxLength(10)]
+        public string cedula { get; set; }
+
+        [MaxLength(50)]
+        public string genero { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        public string getNameValue() {
+            return this.primerNombre + this.primerApellido;
+        }
+    }
+}
